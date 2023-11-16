a05054e97930a3f6d3f0fee3c77c4561bc6f4079
Dominibo95 <dominibo95@gmail.com>
2023-11-16 14:00:17 +0000

    BAse for app

---
+++ DailyIncome/DailyIncome/IJob.cs
@@ -0,0 +1,17 @@
+﻿namespace DailyIncome
+{
+    public interface IJob
+    {
+        int JobId { get; }
+
+        //dodaje obiekt task do obiektu job 
+       // void AddTask();
+
+
+
+
+
+       // event JobAddedDelegate JobAdded;
+        Statistics GetStatistics { get; }
+    }
+}
---
+++ DailyIncome/DailyIncome/Statistics.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace DailyIncome
+{
+    public class Statistics
+    {
+
+        //Job with lowest Wage
+        public double MinWagePerJob { get; private set; }
+
+        // job with highest wage 
+        public double MaxWagePerJob { get; private set; }
+
+        public double Sum { get; private set; }
+
+        public double AverageDailyPerHour
+        {
+            get
+            {
+                return this.Sum / 8;
+            }
+        }
+
+        public Statistics()
+        {
+            this.Sum = 0;
+            this.MaxWagePerJob = double.MinValue;
+            this.MinWagePerJob = double.MaxValue;
+        }
+
+        public void AddJob(double job)
+        {
+            this.Sum += job;
+            this.MinWagePerJob = Math.Min(job, this.MinWagePerJob);
+            this.MaxWagePerJob = Math.Max(job, this.MaxWagePerJob);
+        }
+    }
+}
